27ba9c78c15b6cf0ed2de4bf1d2729c2c84e38b7
eysteinn13 <eysteinngu@gmail.com>
2015-09-04 04:15:19 -0700

    service entity classes

--- API.Services/Entities/Course.cs
+++ API.Services/Entities/Course.cs
@@ -7,13 +7,33 @@ using System.Threading.Tasks;
 
 namespace API.Services.Entities
 {
+    /// <summary>
+    /// This class represents a course template in the database
+    /// </summary>
     [Table("Courses")]
     class Course
     {
+        /// <summary>
+        /// A unique identifier for the course
+        /// </summary>
         public int ID { get; set; }
+        /// <summary>
+        /// The template id for a course. Foreign key to the CourseTemplate table
+        /// Example "T-504-VEFT"
+        /// </summary>
         public string TemplateID { get; set; }
+        /// <summary>
+        /// The start date for a course
+        /// </summary>
         public DateTime StartDate { get; set; }
+        /// <summary>
+        /// The end date for a course
+        /// </summary>
         public DateTime EndDate { get; set; }
+        /// <summary>
+        /// The semester and year the course is taught in
+        /// Example "201501"
+        /// </summary>
         public string Semester { get; set; }
     }
 }
