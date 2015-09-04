bda52abc5c2e4e1611e942d74d6215fec5c0058d
eysteinn13 <eysteinngu@gmail.com>
2015-09-04 03:08:48 -0700

    merge

--- API.Models/CourseViewModel.cs
+++ API.Models/CourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace API.Models
         /// <summary>
         /// 
         /// </summary>
-        // [Required]
+        [Required]
         public int CouresID { get; set; }
 
         /// <summary>
